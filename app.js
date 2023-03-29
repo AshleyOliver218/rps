8764cd75db36d95b959456bf1bcd048f882ba4d3
Ashley Oliver <jeangray218@gmail.com>
2023-03-29 16:25:29 -0400

    js and html score board fixes and minor css styling

--- app.js
+++ app.js
@@ -4,11 +4,16 @@
 //P>R
 let playerScore = 0
 let compScore = 0
+let humanScore = document.querySelector("#human-score")
+let computerScore = document.querySelector("#computer-score")
+humanScore.textContent = playerScore
+computerScore.textContent = compScore
 //click picture to select choices
 //add event listener playerChoice: rock
 let setPlayerChoiceAsRock = document.querySelector(".playersChoiceRock")
 setPlayerChoiceAsRock.addEventListener('click', () => {
     let playerChoice = "Rock"
+    setPlayerChoiceAsRock.style.border = "4px dotted blue"
     gameLogic(playerChoice)
 
 })
@@ -16,6 +21,7 @@ setPlayerChoiceAsRock.addEventListener('click', () => {
 let setPlayerChoiceAsScissors = document.querySelector(".playersChoiceScissors")
 setPlayerChoiceAsScissors.addEventListener('click', () => {
     let playerChoice = "Scissors"
+    setPlayerChoiceAsScissors.style.border = "4px dotted blue"
     gameLogic(playerChoice)
 })
 
@@ -23,15 +29,22 @@ setPlayerChoiceAsScissors.addEventListener('click', () => {
 let setPlayerChoiceAsPaper = document.querySelector(".playersChoicePaper")
 setPlayerChoiceAsPaper.addEventListener('click', () => {
     let playerChoice = "Paper"
+    setPlayerChoiceAsPaper.style.border = "4px dotted blue"
     gameLogic(playerChoice)  
 })
 
+// let setPlayerChoiceAsRockHover = document.querySelector(".playersChoiceRock")
+// setPlayerChoiceAsRockHover.addEventListener('mouseover', () => {
+//     let playerChoice = "Rock"
+//     gameLogic(playerChoice)
+
+// })  
 //fucntion to run game
 let gameLogic = (playerChoiceParam) => {
 
 
     let playerChoice = playerChoiceParam
-
+//creating computers random choice
     let choiceBank = ["Rock", "Paper", "Scissors"]
     let setCompChoice = choiceBank[Math.floor(Math.random() * 3)]
 
@@ -39,47 +52,54 @@ let gameLogic = (playerChoiceParam) => {
 
     let compChoice = setCompChoice
 
+//player win conditions
     if (playerChoice === "Rock" && compChoice === "Scissors") {
         console.log("You Win Round")
+        setPlayerChoiceAsPaper.style.border = "0"
+        setPlayerChoiceAsScissors.style.border = "0"
         playerScore++
-        console.log(playerScore)
-        console.log(compScore)
-        
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     }
     if (playerChoice === "Scissors" && compChoice === "Paper") {
         console.log("You Win Round")
+        setPlayerChoiceAsPaper.style.border = "0"
+        setPlayerChoiceAsRock.style.border = "0"
         playerScore++
-        console.log(playerScore)
-        console.log(compScore)
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     }
     if (playerChoice === "Paper" && compChoice === "Rock") {
         console.log("You Win Round")
+        setPlayerChoiceAsRock.style.border = "0"
+        setPlayerChoiceAsScissors.style.border = "0"
         playerScore++
-        console.log(playerScore)
-        console.log(compScore)
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     }
 
 
 
-    //lose conditions   
+    //player lose conditions   
 
     if (playerChoice === "Rock" && compChoice === "Paper") {
         console.log("The Computer Wins Round")
         compScore++
-        console.log(playerScore)
-        console.log(compScore)
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
+        
     }
     if (playerChoice === "Scissors" && compChoice === "Rock") {
         console.log("The Computer Wins Round")
         compScore++
-        console.log(playerScore)
-        console.log(compScore)
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     }
     if (playerChoice === "Paper" && compChoice === "Scissors") {
         console.log("The Computer Wins Round")
         compScore++
-        console.log(playerScore)
-        console.log(compScore)
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     }
 
 
@@ -95,16 +115,31 @@ let gameLogic = (playerChoiceParam) => {
     }
     console.log(`You chose ${playerChoice}`)
     console.log(`The Computers chose ${compChoice}`)
-
+    //Score Conditions to make "Game Over"
     if(playerScore >= 2){
-        console.log("You win! Game Over")
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
+        alert("You win! Game Over. click okay to restart")
+        setPlayerChoiceAsRock.style.border = "0"
+        setPlayerChoiceAsPaper.style.border = "0"
+        setPlayerChoiceAsScissors.style.border = "0"
         playerScore = 0
         compScore = 0
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
     } 
     if(compScore >= 2){
-        console.log("The Computer Wins! Game Over")
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
+        alert("The Computer Wins! Game Over. click okay to restart")
+        setPlayerChoiceAsRock.style.border = "0"
+        setPlayerChoiceAsPaper.style.border = "0"
+        setPlayerChoiceAsScissors.style.border = "0"
         playerScore = 0
         compScore = 0
+        humanScore.textContent = playerScore
+        computerScore.textContent = compScore
+        
     }
 }
 
